5633b13c5d75e1bd71a2cbe62cf6fca394cc9715
JAreina <jareinafdez@gmail.com>
2018-01-11 00:18:12 +0100

    i18n

--- 3-ui--Angular4.2--a--Angular5.2/src/app/app.module.ts
+++ 3-ui--Angular4.2--a--Angular5.2/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+
+import { getLocaleProvider } from "./i18n-providers";
 import { FormsModule } from '@angular/forms';
 import {RutasRoutingModule } from './rutas/rutas-routing.module';
 
@@ -28,7 +30,7 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
     PostsModule,   // PostComponente, y PostsComponente
     NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: getLocaleProvider } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- 3-ui--Angular4.2--a--Angular5.2/src/app/componentes/posts/post/post.component.ts
+++ 3-ui--Angular4.2--a--Angular5.2/src/app/componentes/posts/post/post.component.ts
@@ -1,35 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'jareina-post',
-  templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  selector: "jareina-post",
+  templateUrl: "./post.component.html",
+  styleUrls: ["./post.component.css"]
 })
-
-
-
 export class PostComponent implements OnInit {
-  ngOnInit(): void {
-   
-  }
+  postDate = new Date().toLocaleString();
+
+  ngOnInit(): void {}
 
   postId = null;
-  constructor(private route:ActivatedRoute,
-                    private router:Router) {
-
+  constructor(private route: ActivatedRoute, private router: Router) {
     // recibe el parametro como un observable
-  route.params.subscribe(
-  params =>{
-  this.postId = parseInt(params['id']);
+    route.params.subscribe(params => {
+      this.postId = parseInt(params["id"]);
 
-  // si el parametro es invalido
-  if (isNaN(this.postId)) {
-    //no muestra en la url /404 skip location 
-    this.router.navigateByUrl("/404",{ skipLocationChange: true })
-    }
+      // si el parametro es invalido
+      if (isNaN(this.postId)) {
+        //no muestra en la url /404 skip location
+        this.router.navigateByUrl("/404", { skipLocationChange: true });
+      }
+    });
   }
-  );
-  }
-
 }
